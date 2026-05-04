dd93a3a7e1ab68d3a56447fe0eb42ae11c142494
zepaulo333 <josep.sousa.369@gmail.com>
2026-05-04 16:47:33 +0100

    eliminei mais  duplicados

--- src/algorithms/adaptive_k/__init__ 2.py
+++
@@ -1,9 +0,0 @@
-from .knn_adaptive_entropy import KNNAdaptiveEntropy
-from .knn_adaptive_eigen import KNNAdaptiveEigen
-try:
-    from .knn_adaptive_topo import KNNAdaptiveTopo
-except ImportError:
-    KNNAdaptiveTopo = None
-from .knn_adaptive_dual_anchor import KNNAdaptiveDualAnchor
-
-__all__ = ["KNNAdaptiveEntropy", "KNNAdaptiveEigen", "KNNAdaptiveTopo", "KNNAdaptiveDualAnchor"]
--- src/algorithms/baseline/__init__ 2.py
+++
@@ -1,6 +0,0 @@
-from .knn_base import KNNClassifier, KNNClassifierFast, KNNOptK
-from .knn_weighted import KNNWeighted
-from .dann import DANN
-from .dann_adaptive import DANNAdaptive
-
-__all__ = ["KNNClassifier", "KNNClassifierFast", "KNNOptK", "KNNWeighted", "DANN", "DANNAdaptive"]
